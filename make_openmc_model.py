22fac7d8c4bd9dd9bf16b0597aade7b21017fa79
lewisgross1296 <ligross@wisc.edu>
2022-09-30 17:40:10 -0500

    switch from cell creation to using openmc.RegularMesh(), lots of TODOs and things to determine still. beginning MGXS code

--- make_openmc_model.py
+++ make_openmc_model.py
@@ -1,71 +1,59 @@
 import openmc
 import openmc.mgxs as mgxs
-
+import numpy as np
 # problem parameters
-T0 = 300
+T0 = 293
 L = 100.0 # TODO determine which L, initial or equilibrium
 infdim = 50.0 # length at which the reflective boundary conditions will be to simulate infiniteness in YZ dimension
-rho = 10.0 # g/cc
+rho = 1.2 # g/cc
 N = 4 # number of regions in the problem
 
-# create materials
-
-slab = openmc.Material(1, "slab")
-# uo2.add_element('U', 1.0, enrichment=5.0) # this will be used in computing lambda/the eigenvalue
-# uo2.add_element('O', 2.0)
-# uo2.set_density('g/cm3',rho)
-# mats = openmc.Materials([uo2])
+# create material for slab
+slab = openmc.Material(1, "slab",T0)
+slab.set_density('g/cm3',rho)
+mats = openmc.Materials([slab])
 mats.export_to_xml()
 
-# TODO maybe change x_N to x_4 or get loop to work lol
-# boundary planes of the problem
-x_0 = openmc.XPlane(x0 = -L/2, boundary_type='vacuum')
-x_N = openmc.XPlane(x0 = L/2, boundary_type='vacuum')
-y_minus = openmc.YPlane(y0 = -infdim, boundary_type='reflective')
-y_plus = openmc.YPlane(y0 = infdim, boundary_type='reflective')
-z_minus = openmc.ZPlane(z0 = -infdim, boundary_type='reflective')
-z_plus = openmc.ZPlane(z0 = infdim, boundary_type='reflective')
-
-
-# TODO see if there's a way to use loops to do this, getting stuck on variable number of variables
-# maybe precreate the correct number and loop to update, but not sure on this
-# x planes to create N x 1 x 1 mesh, make
-# using N = 4, so there need to be 3 internal planes
-# most likely need more than 4 for the real deal, which motivates a loop
-x_1 = openmc.XPlane(x0 = -L/4 , boundary_type='transmission')
-x_2 = openmc.XPlane(x0 = 0 , boundary_type='transmission')
-x_3 = openmc.XPlane(x0 = L/4, boundary_type='transmission')
-
-
-# create regions
-region_1 = +x_0 & -x_1 & +y_minus & -y_plus & +z_minus & -z_plus
-region_2 = +x_1 & -x_2 & +y_minus & -y_plus & +z_minus & -z_plus
-region_3 = +x_2 & -x_3 & +y_minus & -y_plus & +z_minus & -z_plus
-region_4 = +x_3 & -x_N & +y_minus & -y_plus & +z_minus & -z_plus
-
-
-# create cells
-cell_1 = openmc.Cell(cell_id=1, fill=uo2, region = region_1 )
-cell_2 = openmc.Cell(cell_id=2, fill=uo2, region = region_2 )
-cell_3 = openmc.Cell(cell_id=3, fill=uo2, region = region_3 )
-cell_4 = openmc.Cell(cell_id=4, fill=uo2, region = region_4 )
-
-geom = openmc.Geometry([cell_1,cell_2,cell_3,cell_4])
-geom.export_to_xml()
-
-# define mesh tally
+# define mesh tally MAYBE DONT SINCE EACH MGXS object has a .tallies to use instead?
 mesh = openmc.RegularMesh()
 mesh.dimension = (N,1,1) # N slices in the x dimension, 1 in y and z dimensions
 mesh.lower_left = (-L/2,-infdim,-infdim)
 mesh.upper_right = (L/2,infdim,infdim)
-mesh_filter = openmc.MeshFilter(mesh)
+# mesh_filter = openmc.MeshFilter(mesh) TBD if we want this
+mesh.build_cells(['vacuum','vacuum','reflective','reflective','reflective','reflective']) # xmin , x max, ymin, ymax, zmin, zmax TODO are the internal ones transmission by default?
+mesh.export_to_xml() # TODO how to export mesh version of geometry is this enough?
 
-tally = openmc.Tally(tally_id=1,name="mesh_tally")
-tally.filters = [mesh_filter]
-tally.scores = ["flux"]
-tallies = openmc.Tallies([tally])
-tallies.export_to_xml()
+# # TODO determine if mesh is better and we create our own cells but save the below just incase
+# # boundary planes of the problem
+# x_0 = openmc.XPlane(x0 = -L/2, boundary_type='vacuum')
+# x_N = openmc.XPlane(x0 = L/2, boundary_type='vacuum')
+# y_minus = openmc.YPlane(y0 = -infdim, boundary_type='reflective')
+# y_plus = openmc.YPlane(y0 = infdim, boundary_type='reflective')
+# z_minus = openmc.ZPlane(z0 = -infdim, boundary_type='reflective')
+# z_plus = openmc.ZPlane(z0 = infdim, boundary_type='reflective')
 
+
+# x_1 = openmc.XPlane(x0 = -L/4 , boundary_type='transmission')
+# x_2 = openmc.XPlane(x0 = 0 , boundary_type='transmission')
+# x_3 = openmc.XPlane(x0 = L/4, boundary_type='transmission')
+
+
+# # create regions
+# region_1 = +x_0 & -x_1 & +y_minus & -y_plus & +z_minus & -z_plus
+# region_2 = +x_1 & -x_2 & +y_minus & -y_plus & +z_minus & -z_plus
+# region_3 = +x_2 & -x_3 & +y_minus & -y_plus & +z_minus & -z_plus
+# region_4 = +x_3 & -x_N & +y_minus & -y_plus & +z_minus & -z_plus
+
+# # create cells
+# cell_1 = openmc.Cell(cell_id=1, fill=uo2, region = region_1 )
+# cell_2 = openmc.Cell(cell_id=2, fill=uo2, region = region_2 )
+# cell_3 = openmc.Cell(cell_id=3, fill=uo2, region = region_3 )
+# cell_4 = openmc.Cell(cell_id=4, fill=uo2, region = region_4 )
+
+# # define universe of cells
+# root_universe = openmc.Universe(name='root universe', cells=[cell_1,cell_2,cell_3,cell_4])
+# geom = openmc.Geometry([cell_1,cell_2,cell_3,cell_4])
+# geom.export_to_xml()
 
 # settings
 settings = openmc.Settings()
@@ -82,15 +70,38 @@ settings.output = {'tallies': True}
 bounds = [-L -infdim, -infdim, L, infdim, infdim]
 uniform_dist = openmc.stats.Box(bounds[:3], bounds[3:], only_fissionable=True)
 settings_file.source = openmc.Source(space=uniform_dist)
-
-
-# set temperatures not sure about this one since we'll likely use a temperature XS lib that we create, but the below may be useful
-# settings.temperature = {'default': T0,
-#                         'method': 'interpolation',
-#                         'range': (294.0, 1600.0)} # good to load all temperatures you could encounter in multiphysics
-
+settings.temperature = {'default': T0,
+                        'method': 'interpolation',
+                        'range': (294.0, 1600.0)} # good to load all temperatures you could encounter in multiphysics
 settings.export_to_xml()
 
 # generate one group cross sections
 groups = mgxs.EnergyGroups()
 groups.group_edges = np.array([0.0, 20.0e6]) #  groups in eV
+
+# domain (openmc.Material or openmc.Cell or openmc.Universe or openmc.RegularMesh) – The domain for spatial homogenization
+# domain_type ({'material', 'cell', 'distribcell', 'universe', 'mesh'}) – The domain type for spatial homogenization
+
+# Question: does specifying domain = root_universe command openmc to compute the XS in each cell?
+total = mgxs.TotalXS(domain=root_universe, groups=groups)
+absorption = mgxs.AbsorptionXS(domain=root_universe, groups=groups)
+scattering = mgxs.ScatterXS(domain=root_universe, groups=groups)
+fission = mgxs.FissionXS(domain=root_universe, groups=groups, nu=True)
+
+# Question how do we get our library in here, especially since I didn't provide element information in the materials
+
+
+
+
+# tally = openmc.Tally(tally_id=1,name="mesh_tally")
+# tally.filters = [mesh_filter]
+# tally.scores = ["flux"]
+# tallies = openmc.Tallies([tally])
+# tallies.export_to_xml()
+
+mgxs_tallies = openmc.Tallies()
+mgxs_tallies += total.tallies.values()
+mgxs_tallies += absorption.tallies.values()
+mgxs_tallies += scattering.tallies.values()
+mgxs_tallies += fission.tallies.values()
+mgxs_tallies.export_to_xml()
