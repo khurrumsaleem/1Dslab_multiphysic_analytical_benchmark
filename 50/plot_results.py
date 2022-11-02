892a4c9af8d3f14d2a052bdaa57f8b99b5b13886
lewisgross1296 <ligross@wisc.edu>
2022-11-02 16:08:37 -0500

    got flux histogram working properly. TODO error bars and error from the analytical solution

--- 50/plot_results.py
+++ 50/plot_results.py
@@ -2,13 +2,34 @@ import openmc
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load statepoint file and get the number of binss
+# Load statepoint file and get the number of bins
 sp = openmc.StatePoint('statepoint.500.h5')
 tally = sp.get_tally()
-bins = tally.shape[0] # this works because the mesh is N x 1 x 1, so the total number of bins is N
-# get scores for each tally type, compute realtive errors
-# flux
+num_voxels = tally.shape[0] # this works because the mesh is N x 1 x 1, so the total number of bins is N
+
+# get scores for each flux
 flux = tally.get_slice(scores=['flux'])
+
+L = 106.47 # slab length from paper
+# plot using hist
+flux.mean.shape = (num_voxels)
+flux_min = min(flux.mean)
+flux_max = max(flux.mean)
+flux.std_dev.shape= (num_voxels)
+bin_boundaries = np.linspace(-L,L,num_voxels+1)
+# give one value in each bin paired with the bin boundaries.
+# then scale the weight of each bin by the flux
+plt.hist(bin_boundaries[:-1],bin_boundaries,weights=flux.mean,histtype='step') # plot flux vs position
+# TODO add error bars for flux bins
+bin_average_values = (bin_boundaries[1:] + bin_boundaries[:-1])/2 # get bin centers
+plt.title('Flux in Each Mesh Element')
+plt.xlabel('Position [cm]')
+plt.ylabel('Flux [n/cm^2-s]')
+plt.xlim([-(L+2),L+2])
+plt.xticks(bin_boundaries[::5])
+plt.ylim([flux_min-0.01,flux_max+0.01])
+plt.savefig('flux_dist_50.png')
+plt.clf()
 # Determine relative error
 flux_relative_error = np.zeros_like(flux.std_dev)
 nonzero = flux.mean > 0
@@ -20,18 +41,5 @@ plt.xlabel('Relative Error')
 plt.ylabel('Frequency')
 plt.savefig('relative_errors_50.png')
 plt.clf()
-# kappa fisison
-kappa_fission = tally.get_slice(scores=['kappa-fission'])
 
-# plot flux vs position
-L = 10 # slab length
-# plot using histogram
-flux.mean.shape = (bins)
-flux.std_dev.shape= (bins)
-space_bins = np.linspace(-L,L,bins)
-# mean,bin_edges = np.histogram(flux.mean,bins=bins)
-plt.step(space_bins,flux.mean)
-plt.title('Flux in Each Mesh Element')
-plt.xlabel('Position')
-plt.ylabel('Flux')
-plt.savefig('flux_dist_50.png')
+# TODO compute error from analytical solutioin
