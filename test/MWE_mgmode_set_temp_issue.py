25913315b5734eb7199156e6baee5db36b97af80
lewisgross1296 <ligross@wisc.edu>
2022-11-15 19:44:30 -0600

    very close to h5 data file with two temps, but still getting issues with data from openmc.mgxs.Library obejct

--- test/MWE_mgmode_set_temp_issue.py
+++ test/MWE_mgmode_set_temp_issue.py
@@ -9,30 +9,38 @@ T0 = 303 # temp in range that should be set-able
 Tmax = 313
 
 # create macroscopic object and export material with temperature XS library generated below
-fuel = openmc.Material(material_id=1,name="fuel")
-fuel.add_element('U', 1., enrichment=3.0)
-fuel.set_density('macro',18.9)
-materials = openmc.Materials([fuel])
+cold_fuel = openmc.Material(material_id=1,name="cold_fuel")
+hot_fuel = openmc.Material(material_id=2,name="hot_fuel")
+# default names for the cross section set used in the CE file to generate the data are set1,set2,...
+# there are two cells in the model and we computed XS for each
+cold_fuel.add_macroscopic('set1')
+hot_fuel.add_macroscopic('set2')
+materials = openmc.Materials([cold_fuel,hot_fuel])
 materials.cross_sections = 'onegxs.h5'
 materials.export_to_xml()
 
 # define geometry
 H = 100.0 # cm
 xplane0 = openmc.XPlane(x0  = 0.0, boundary_type='vacuum')
+xmidplane = openmc.XPlane(x0  = H/2, boundary_type='transmission')
 xplane1 = openmc.XPlane(x0 = H, boundary_type='vacuum')
 yplane0 = openmc.YPlane(y0 = 0.0, boundary_type='vacuum')
 yplane1 = openmc.YPlane(y0 = H, boundary_type='vacuum')
 zplane0 = openmc.ZPlane(z0 = 0.0, boundary_type='vacuum')
 zplane1 = openmc.ZPlane(z0 = H, boundary_type='vacuum')
-cube_region = +xplane0 & -xplane1 & +yplane0 & -yplane1 & +zplane0 & -zplane1
-root_cell = openmc.Cell(cell_id=0, region=cube_region, fill=fuel)
-geometry = openmc.Geometry([root_cell])
+cold_region = +xplane0 & -xmidplane & +yplane0 & -yplane1 & +zplane0 & -zplane1
+hot_region = +xmidplane & -xplane1 & +yplane0 & -yplane1 & +zplane0 & -zplane1
+cold_cell = openmc.Cell(cell_id=0, region=cold_region, fill=cold_fuel)
+cold_cell.temperature = Tmin
+hot_cell = openmc.Cell(cell_id=1, region=hot_region, fill=hot_fuel)
+hot_cell.temperature = Tmax
+geometry = openmc.Geometry([cold_cell,hot_cell])
 geometry.export_to_xml()
 
 # define settings
 settings = openmc.Settings()
-batches = 200
-inactive = 50
+batches = 20
+inactive = 10
 particles = 5000
 settings.energy_mode = 'multi-group'
 settings.batches = batches
--- test/generator/mgxs_generator.py
+++ test/generator/mgxs_generator.py
@@ -22,14 +22,19 @@ model.materials = materials
 # define geometry
 H = 100.0 # cm
 xplane0 = openmc.XPlane(x0  = 0.0, boundary_type='vacuum')
+xmidplane = openmc.XPlane(x0  = H/2, boundary_type='transmission')
 xplane1 = openmc.XPlane(x0 = H, boundary_type='vacuum')
 yplane0 = openmc.YPlane(y0 = 0.0, boundary_type='vacuum')
 yplane1 = openmc.YPlane(y0 = H, boundary_type='vacuum')
 zplane0 = openmc.ZPlane(z0 = 0.0, boundary_type='vacuum')
 zplane1 = openmc.ZPlane(z0 = H, boundary_type='vacuum')
-cube_region = +xplane0 & -xplane1 & +yplane0 & -yplane1 & +zplane0 & -zplane1
-root_cell = openmc.Cell(cell_id=0, region=cube_region, fill=fuel)
-geometry = openmc.Geometry([root_cell])
+cold_region = +xplane0 & -xmidplane & +yplane0 & -yplane1 & +zplane0 & -zplane1
+hot_region = +xmidplane & -xplane1 & +yplane0 & -yplane1 & +zplane0 & -zplane1
+cold_cell = openmc.Cell(cell_id=0, region=cold_region, fill=fuel)
+cold_cell.temperature = Tmin
+hot_cell = openmc.Cell(cell_id=1, region=hot_region, fill=fuel)
+hot_cell.temperature = Tmax
+geometry = openmc.Geometry([cold_cell,hot_cell])
 model.geometry = geometry
 
 # define settings
@@ -59,9 +64,9 @@ onegxs_lib.energy_groups = groups
 onegxs_lib.mgxs_types = ['total', 'absorption', 'nu-fission', 'fission',
                        'nu-scatter matrix', 'multiplicity matrix', 'chi']
 # Specify a "cell" domain type for the cross section tally filters
-onegxs_lib.domain_type = "material"
+onegxs_lib.domain_type = "cell"
 # Specify the cell domains over which to compute multi-group cross sections
-onegxs_lib.domains = geometry.get_all_materials().values()
+onegxs_lib.domains = geometry.get_all_cells().values()
 # Set the Legendre order to 3 for P3 scattering
 onegxs_lib.legendre_order = 3
 # check library and build
@@ -78,5 +83,21 @@ statepoint = openmc.StatePoint(sp, autolink=True)
 # merge tallies and load from statepoint
 onegxs_lib.load_from_statepoint(statepoint)
 onegxs_file = onegxs_lib.create_mg_library(xs_type='macro')
-# onegxs_file.export_to_hdf5()
-onegxs_lib.build_hdf5_store(filename='onegxs.h5',directory="/home/lgross/1Dslab_multiphysic_analytical_benchmark/test")
+
+
+# NEED something like this,
+# xsdata = openmc.XSdata(energy_groups = groups,temperatures = np.([[Tmin,Tmax]]))
+# xsdata.add_data(onegxs_lib.get_xsdata())
+
+# this almost workrs
+# grab data from openmc.mgxs.Library and set tempearture of each xsdata object
+xsdata_cold = onegxs_lib.get_xsdata(xsdata_name='set1',domain=cold_cell)
+xsdata_cold.temperature = Tmin
+xsdata_hot = onegxs_lib.get_xsdata(xsdata_name='set2',domain=hot_cell)
+xsdata_hot.temperature = Tmax
+print(xsdata_cold.temperature)
+print(xsdata_hot.temperature)
+
+temp_lib = openmc.MGXSLibrary(groups)
+temp_lib.add_xsdatas([xsdata_cold,xsdata_hot])
+temp_lib.export_to_hdf5(filename='/home/lgross/1Dslab_multiphysic_analytical_benchmark/test/onegxs.h5')
