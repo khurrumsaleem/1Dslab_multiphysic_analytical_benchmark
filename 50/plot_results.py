bccf3ce504e053cf9a3c256e6d0ecaaf87aa26f2
lewisgross1296 <ligross@wisc.edu>
2022-11-28 20:54:05 -0600

    added source strength. TODO determine if flux is just not converged / if volume needs inclusion

--- 50/plot_results.py
+++ 50/plot_results.py
@@ -2,24 +2,38 @@ import openmc
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load statepoint file and get the number of bins
-sp = openmc.StatePoint('statepoint.500.h5')
-tally = sp.get_tally()
-num_voxels = tally.shape[0] # this works because the mesh is N x 1 x 1, so the total number of bins is N
-# get scores for each flux
-flux = tally.get_slice(scores=['flux'])
-
 # paramters for analytical solution
 T0 = 293
-L = 106.47 # equilibrium length from paper (TODO perhaps use formula)
+L = 106.47 # equilibrium length from paper
+infdim = 50.0 # length at which the reflective boundary conditions will be to simulate infiniteness in YZ dimension
 P = 1.0e22 # eV/s
 q = 1e8 # eV
 k0= 1.25e19 # eV/(s-cm-K^2) k(T) = k0 T(x)
 phi0 = 2.5e14 # 1/s-cm^2 flux at the origin
 eV_to_J = 1.602e-19 # J per eV
 lam = 0.5*(1+np.sqrt(1+(16*q*q*phi0*phi0)/(P*P)))
+
+# Load statepoint file and get the number of bins
+sp = openmc.StatePoint('statepoint.300.h5')
+
+# grab mesh tally from tally 1 get scores for each flux
+tally_1 = sp.get_tally()
+num_voxels = tally_1.shape[0] # this works because the mesh is N x 1 x 1, so the total number of bins is N
+flux = tally_1.get_slice(scores=['flux'])
+
+# grab tally 2 system wide, volume integrated kappa fission rate
+tally_2 = sp.get_tally(id=2)
+system_tallied_power = tally_2.get_slice(scores=['kappa-fission'])
+system_tallied_power.mean.shape = (1)
+# print(system_tallied_power.mean)
 # plot flux data using hist
 flux.mean.shape = (num_voxels)
+# correct by the source strength and voxel volume
+V_voxel = (L*infdim*infdim)/num_voxels
+print(V_voxel)
+c = P/system_tallied_power.mean[0]
+print("c = ",c)
+flux = c*flux#/V_voxel TODO figure out about V_voxel, tbh could just be unconverged flux?
 flux_min = min(flux.mean)
 flux_max = max(flux.mean)
 flux.std_dev.shape= (num_voxels)
@@ -59,4 +73,3 @@ plt.xlabel('Position [cm]')
 plt.ylabel('Flux [n/cm^2-s]')
 plt.title('Analytical Flux for 50 Mesh Points')
 plt.savefig('analytical_50.png')
-#TODO scaling from total kappa fission tally to turn flux tally into correct units
