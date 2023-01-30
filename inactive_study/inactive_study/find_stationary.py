e491d198952688c555883648b678467e3d217916
lewisgross1296 <ligross@wisc.edu>
2023-01-30 15:29:36 -0600

    added argument parsing for either all, half, or window method as well as implementing logic/looping. TODO decide if we should throw away a certain number of initial samples and also whether to add that as an optional parameter

--- inactive_study/inactive_study/find_stationary.py
+++ inactive_study/inactive_study/find_stationary.py
@@ -1,7 +1,22 @@
+from argparse import ArgumentParser
 import numpy as np
 import openmc
 import sys
 import statistics as stat
+
+# collect argument for type of stationary detection: window, all, last half
+ap = ArgumentParser(description="Program to analyze Shannon entropy data and detect convergence of the fission source.")
+ap.add_argument('--detector', dest = 'detector', choices =['all','half','window'], required=True,
+                help = "The type of way to detect steady state. Options are all, half, or window")
+opts, rem_args = ap.parse_known_args()
+if(opts.detector == "window"):
+    ap.add_argument('--window_length', dest = 'window_length',required = True, type = int,
+                    help =" The window length must be specified if window is the detectio method")
+args = ap.parse_args()
+# variable to be used in logic for which way to detect steady state
+method = args.detector
+if(args.detector == "window"):
+    window_length = args.window_length
 
 # eigenvalue batching
 n_inactive = 250
@@ -32,26 +47,52 @@ with openmc.StatePoint(sp_file) as sp:
     averaging_k = sp.k_generation[(n_inactive - averaging_batches):n_inactive]
     k_generation_avg.append(sum(averaging_k) / len(averaging_k))
 
-# detecting stationarity. Accordinig to Brown, the below is done in MCNP5:
-# find the first cycle when Hsrc is within one standard deviation of its average for the last half of cycles
-# TODO decide whether to use this, all points, or a windowed average
-window_length = 10
-# detect when entropy of current batch is within window (mean +/- std of window)
-for j in range(window_length,len(entropy[0])):
-    window = entropy[0][(j-window_length):j]
-    window_mean = np.average(window)
-    window_dev = np.std(window)
-    window_low = window_mean - window_dev
-    window_high = window_mean + window_dev
-    # print("window bounds: ", window_low , " to ", window_high )
-    if(window_high - entropy[0][j] > 0 and entropy[0][j]-window_low > 0):
-        # if entropy[j] is less than window_high and greater than
-        # window_low, then it is within the window and we've succeeded
-        print("Iteration ", j, " produced a value in the window. It is recommended "
-              "to do at least this many inactive cycles.")
-        break
-    else:
-        continue
-
-# compute on the fly the average of the eigenvalue (last half batches) +/- the std dev of the last half cycles
-# and see if the current k is within this interval
+# TODO maybe add parameter to throw out certain number of starting values and go from that instead of 1
+# detecting stationarity.
+if(method == "window"):
+    # detect when entropy of current batch is within window (mean +/- std of window)
+    for j in range(window_length,len(entropy[0])):
+        window = entropy[0][(j-window_length):j]
+        window_mean = np.average(window)
+        window_dev = np.std(window)
+        window_low = window_mean - window_dev
+        window_high = window_mean + window_dev
+        # print("window bounds: ", window_low , " to ", window_high )
+        if(window_high - entropy[0][j] > 0 and entropy[0][j]-window_low > 0):
+            # if entropy[j] is less than window_high and greater than
+            # window_low, then it is within the window and we've succeeded
+            print("Iteration ", j, " produced a value in the window. It is recommended "
+                "to do at least this many inactive cycles.")
+            break
+        else:
+            continue
+elif(method == "half"):
+    #  Accordinig to Brown, the below is done in MCNP5:
+    # find the first cycle when Hsrc is within one standard deviation of its average for the last half of cycles
+    # compute on the fly the average of the eigenvalue (last half batches) +/- the std dev of the last half cycles
+    # and see if the current k is within this interval
+    for j in range(1,len(entropy[0])):
+        last_half_idx = int(np.floor(j/2))
+        last_half_vals = entropy[0][last_half_idx:j]
+        stdev = np.std(last_half_vals)
+        mean = np.mean(last_half_vals)
+        low =  mean - stdev
+        high = mean + stdev
+        if(high - entropy[0][j] > 0 and entropy[0][j]-low > 0):
+            print("Iteration", j, "produced a value within one standard deviation of the mean for the last half of entropy values.")
+            break
+        else:
+            continue
+else: 
+    # use all batches as data points
+    for j in range(1,len(entropy[0])):
+        entropy_slice = entropy[0][0:j]
+        stdev = np.std(entropy_slice)
+        mean = np.mean(entropy_slice)
+        low =  mean - stdev
+        high = mean + stdev
+        if(high - entropy[0][j] > 0 and entropy[0][j]-low > 0):
+            print("Iteration", j, "produced a value within one standard deviation of the mean of the last j entropy values")
+            break
+        else:
+            continue
