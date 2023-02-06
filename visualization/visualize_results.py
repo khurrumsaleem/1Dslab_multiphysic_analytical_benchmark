59b9b66b5563874a4ee4ee4977a8d5604434297f
lewisgross1296 <ligross@wisc.edu>
2023-02-06 14:39:56 -0600

    starter for data viz

---
+++ visualization/visualize_results.py
@@ -0,0 +1,12 @@
+import openmc
+import numpy as np
+import matplotlib.pyplot as plt
+
+# mesh size vs error
+# collect norm data to plot element size vs computed error
+# use csv parser to get the final error and make a simple plot
+
+# plotting flux distributions
+# iterate through available statpoint files, grab the flux distribution from each
+# add them to a list of sorts and plot them on one plot
+
